6a21e716d495f1fa6615c2b55832941371d5ca93
Patrick Tran (Pat-GPT) <67556421+patricktran1@users.noreply.github.com>
2026-07-14 17:34:52 -0700

    Bootstrap local and cloud record providers

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import { AuthProvider } from './context/AuthContext'
+import { CloudSyncProvider } from './context/CloudSyncContext'
 import { VitalProvider } from './context/VitalContext'
+import { hydrateSessionFromLocalRecord } from './lib/recordStorage'
 import './styles/index.css'
+
+hydrateSessionFromLocalRecord()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <VitalProvider>
-        <App />
-      </VitalProvider>
+      <AuthProvider>
+        <VitalProvider>
+          <CloudSyncProvider>
+            <App />
+          </CloudSyncProvider>
+        </VitalProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
